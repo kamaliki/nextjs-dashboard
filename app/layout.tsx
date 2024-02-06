1807cb6742419be9db9d681b8f6e8494a2fa5ec7
Kamaliki Clive <47914903+kamaliki@users.noreply.github.com>
2024-02-06 21:45:25 +0300

    more metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
 import { Metadata } from 'next';
+import Image from 'next'
+
 
 export const metadata: Metadata = {
   title: {
@@ -8,12 +10,21 @@ export const metadata: Metadata = {
     default: 'Acme Dashboard',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  metadataBase: new URL('https://nextjs-dashboard-ten-nu-59.vercel.app/'),
   openGraph : {
     title: 'Acme Dashboard',
     description: 'The official Next.js Learn Dashboard built with App Router.',
-    url: 'https://next-learn-dashboard.vercel.sh',
+    url: 'https://nextjs-dashboard-ten-nu-59.vercel.app/',
     siteName: 'Acme Dashboard',
+    images: [
+      {
+        url :"https://nextjs-dashboard-ten-nu-59.vercel.app/opengraph-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
   }
   
 };
